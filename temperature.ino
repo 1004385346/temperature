62727142e43985b9f775b6fb3fa7579d5a9b3b86
1004385346 <1004385346@qq.com>
2015-12-09 17:21:40 +0800

    温度传感器
     通过
    Serial.begin(9600)语句利用9600速率进行串口通讯。定义n变量来读取a3口的电压值。在定义浮点型数来储存温度值，温度值通过电压
    值换算得。然后输出数据。

--- temperature.ino
+++ temperature.ino
@@ -1,14 +1,14 @@
 void setup() {
  
-  Serial.begin(9600);         //使用9600速率进行串口通讯
+  Serial.begin(9600);         
 }
  
 void loop() {
  
-  int n = analogRead(A0);    //读取A0口的电压值
+  int n = analogRead(A3);    
  
-  float vol = n * (5.0 / 1023.0*100);   //使用浮点数存储温度数据，温度数据由电压值换算得到
+  float vol = n * (5.0 / 1023.0*100);   
   Serial.  print("vol="); 
-  Serial.  println(vol);                   //串口输出温度数据
-  delay(2000);                           //等待2秒，控制刷新速度
+  Serial.  println(vol);                   
+  delay(2000);                           
 }
